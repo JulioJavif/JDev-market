875ad38acbae351029e023b5696a54f464ce4729
julio fuentes <juliojavif@gmail.com>
2022-08-02 01:13:23 -0500

    first commit

---
+++ src/main/java/com/platzi/JDevmarket/JDevMarketApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.JDevmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JDevMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JDevMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platzi/JDevmarket/JDevMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.JDevmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JDevMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
