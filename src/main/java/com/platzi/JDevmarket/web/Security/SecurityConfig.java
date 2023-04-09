e8964426f1d621aecb8bcefbe296cd4771531d7b
Julio Fuentes <juliojavif@gmail.com>
2023-04-09 00:49:10 -0500

    add security and config for pdn

--- src/main/java/com/platzi/JDevmarket/web/Security/SecurityConfig.java
+++ src/main/java/com/platzi/JDevmarket/web/Security/SecurityConfig.java
@@ -1,10 +1,17 @@
 package com.platzi.JDevmarket.web.Security;
 
 import com.platzi.JDevmarket.domain.services.UserSecurityService;
+import com.platzi.JDevmarket.web.Security.filter.JwtFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -12,10 +19,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserSecurityService userSecurityService;
 
+    @Autowired
+    private JwtFilterRequest jwtFilterRequest;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(
                 userSecurityService
         );
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
+                .anyRequest().authenticated().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "configuration/ui", "/swagger-resources/**", "/configuration/security", "/swagger-ui.html",
+                "/webjars/**", "/swagger-ui/**");
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
